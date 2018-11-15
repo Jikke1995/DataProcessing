b5a1a1496741c69942eff7cd543c992942df7680
Jikke1995 <jikke.vdende@hotmail.com>
2018-11-15 13:11:56 +0100

    good files

--- Homework/Week_2/eda.py
+++ Homework/Week_2/eda.py
@@ -2,5 +2,22 @@
 # Name: Jikke van den Ende
 # Student number: 10787593
 """
-This script ... 
+This script ...
 """
+
+import csv
+import pandas as pd
+from requests import get
+from requests.exceptions import RequestException
+from contextlib import closing
+from bs4 import BeautifulSoup
+
+INPUT_CSV = 'input.csv'
+
+def load_file(file):
+    file = pd.read_csv(file)
+    return file
+
+if __name__ == "__main__":
+    data = load_file(INPUT_CSV)
+    print(data)
