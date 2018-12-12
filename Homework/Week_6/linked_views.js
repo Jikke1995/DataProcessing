520e2b6ebe0436bf1e5bbe3be3cea9bd6af47031
Jikke1995 <jikke.vdende@hotmail.com>
2018-12-12 14:55:46 +0100

    update

--- Homework/Week_6/linked_views.js
+++ Homework/Week_6/linked_views.js
@@ -4,20 +4,28 @@ Student number: 10787593
 */
 
 window.onload = function() {
-    createMap();
 
     d3v5.json("data.json").then(function(data){
         console.log(data);
-        console.log(data["AUS"]["2015"])
-        makeBarChart(data, 'BEL');
+        createMap(data);
+        makeBarChart(data, 'LUX');
     });
 
 };
 
+function createDataDatamap(data) {
+  dataDatamap = {}
+  Object.keys(data).forEach(function(key) {
+
+      dataDatamap[key] = data[key]["2015"];
+  });
+
+  return dataDatamap;
+}
+
 function makeBarChart(data, country) {
 
     var chart_width = 400;
-    console.log(chart_width);
     var chart_height = 300;
     var padding = (100 / 2);
     var barpadding = 2;
@@ -26,6 +34,7 @@ function makeBarChart(data, country) {
               .append("svg")
               .attr("width", 500)
               .attr("height", 400)
+              .attr("transform", "translate(100, 100)");
 
     var country_info = data[country];
     var values = [];
@@ -35,8 +44,6 @@ function makeBarChart(data, country) {
         years.push(key);
     });
 
-    console.log(values);
-    console.log(years);
     // Create the sscale function for the x values of the bars
     var xScale = d3v5.scaleTime()
                     .domain([new Date(years[0], 0), new Date((years[years.length - 1]), 0)])
@@ -57,13 +64,32 @@ function makeBarChart(data, country) {
                         .enter()
                         .append("rect")
 
+    var tip = createToolTip(svg);
+
     rectangles.attr("x", function(d, i) {
                   return padding + barpadding + i * (chart_width / values.length);
               })
               .attr("y", function(d) { return padding + chart_height - yScale(d);})
-              .attr("fill", function(d) { return "rgb(3,0, " + (d * 10) + ")"})
+              .attr("fill", function(d) { return "rgb(252,141,89)" })
               .attr("width", chart_width / values.length - barpadding)
-              .attr("height", function(d) { return yScale(d); });
+              .attr("height", function(d) { return yScale(d); })
+              .on('mouseover', function(d,i) {
+                  tip.style("display", null)
+              })
+              .on('mouseout', function(d) {
+                  tip.style("display", "none")
+                  d3v5.select(this).attr("fill", function(d) {
+                  return "rgb(252,141,89)"
+                });
+              })
+              .on('mousemove', function(d, i) {
+                d3v5.select(this).attr("fill", "rgb(252,223,89)")
+                tip.style("display", null);
+                var xPosition = d3v5.mouse(this)[0] - 100;
+                var yPosition = d3v5.mouse(this)[1] - 70;
+                tip.attr("transform", "translate(" + xPosition + "," + yPosition + ")");
+                tip.select("text").text("Alcohol consumption: " + d + ", Year: " + years[i]);
+              });
 
     //Add x-axis
     var x_axis = svg.append("g")
@@ -81,21 +107,57 @@ function makeBarChart(data, country) {
                     .call(d3v5.axisLeft(yScaleAxis))
 
     svg.append("text")
+        .attr("class", "x-label")
         .attr("transform","translate(" + (padding + chart_width / 2)  + "," + (padding + chart_height + padding - 10) + ")")
         .style("text-achor", "middle")
         .text("Year");
 }
 
-function createMap() {
+function createMap(data) {
+
+  dataset = createDataDatamap(data)
+  console.log(dataset);
+
   var map = new Datamap({
     element: document.getElementById('container'),
-    // bubblesConfig: {
-    //   popupTemplate: function(geography, data) {
-    //
-    //     return
-    //   }
-    // }
+    // data: {
+    //   return dataset
+    // },
+    geographyConfig: {
+        popupTemplate: function(geo, dataset) {
+            return ['<div class="hoverinfo"><strong>',
+                    geo.properties.name,
+                     // ': ' + dataset["AUS"],
+                    '</strong></div>'].join('');
+        }
+    }
   });
+}
+
+function createToolTip(svg) {
+  var tip = svg.append("g")
+                .attr("class", "tooltip")
+                .style("display", "none")
+
+  // Append a rect to the tooltip
+  tip.append("rect")
+      .attr("width", 260)
+      .attr("position", "absolute")
+      .attr("height", 20)
+      .attr("fill", "white")
+      .style("opacity", 0.5);
+
+  // Append the possibility for a piece of text for the tooltip
+  tip.append("text")
+      .attr("x", 20)
+      .attr("dy", "1.2em")
+      .attr("position", "absolute")
+      .style("text-achor", "middle")
+      .attr("font-size", "12px")
+      .attr("font-weight", "bold")
+
+  return tip;
+
 }
 
 function getData() {
